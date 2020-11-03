f048f84b12646a3182e792d3463352356c63d8f1
n-v-amstel <niekvana@gmail.cm>
2020-11-03 12:21:17 +0100

    Base_model_niek is now same as gate_planning_base_model. old_base_model_Niek is initial/old model from Niek

--- base_model_niek.py
+++ base_model_niek.py
@@ -1,28 +1,77 @@
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
+import pandas as pd
 
-# distance to each gate
-gate, distance = gp.multidict({
-    'Gate1': 100,
-    'Gate2': 200,
-    'Gate3': 300,
-    })
+gate_sheet_import = pd.read_excel('Gate_Planning.xlsx', sheet_name='Gates')
+flight_sheet_import = pd.read_excel('Gate_Planning.xlsx', sheet_name='Flight Schedule')
 
-# capacity of each flight
-flight, pax, arrival, departure = gp.multidict({
-    'Flight1': [100, 9.00, 9.45],
-    'Flight2': [200, 9.30, 10.15],
-    'Flight3': [300, 10.00, 10.45],
-    })
+gate_import = gate_sheet_import['Gate']
+distance_import = gate_sheet_import['Walking Distance']
+comp_ac_import = gate_sheet_import['Comp. AC']
+gate_type_import = gate_sheet_import['Type']
+gate_security_import = gate_sheet_import['Security']
+flight_import = flight_sheet_import['Flight No.']
+PAX_import = flight_sheet_import['Pax']
+ETA_import = flight_sheet_import['ETA']
+ETD_import = flight_sheet_import['ETD']
+AC_import = flight_sheet_import['AC']
+sec_in_import = flight_sheet_import['Security In']
+sec_out_import = flight_sheet_import['Security Out']
+flight_type_import = flight_sheet_import['Type']
 
+types = np.array([['FSNC', 'Jet Bridge'], ['Low-Cost', 'Remote'], ['Business', 'Business']])
 
-# aircraft availability
-availability = gp.tuplelist([
-    ('Flight1', 'Gate1'), ('Flight1', 'Gate2'), ('Flight1', 'Gate3'),
-    ('Flight2', 'Gate1'), ('Flight2', 'Gate2'), ('Flight2', 'Gate3'),
-    ('Flight3', 'Gate1'), ('Flight3', 'Gate2'), ('Flight3', 'Gate3')
-    ])
+gate = []
+distance = {}
+comp_ac = {}
+gate_type = {}
+gate_security = {}
+
+for i in range(len(gate_import)):
+    gate_x = gate_import[i]
+    gate.append(gate_x)
+    distance[gate_x] = distance_import[i]
+    comp_ac[gate_x] = comp_ac_import[i]
+    gate_type[gate_x] = gate_type_import[i]
+    gate_security[gate_x] = gate_security_import[i]
+
+flight = []
+PAX = {}
+ETA = {}
+ETD = {}
+AC = {}
+flight_type = {}
+sec_in = {}
+sec_out = {}
+
+for j in range(len(flight_import)):
+    flight_x = flight_import[j]
+    flight.append(flight_x)
+    PAX[flight_x] = PAX_import[j]
+    ETA[flight_x] = ETA_import[j]
+    ETD[flight_x] = ETD_import[j]
+    AC[flight_x] = AC_import[j]
+    flight_type[flight_x] = flight_type_import[j]
+    sec_in[flight_x] = sec_in_import[j]
+    sec_out[flight_x] = sec_out_import[j]
+
+errorobj = {}
+for flight1 in flight:
+    error = []
+    for g in range(len(comp_ac_import)):
+        if AC[flight1] in comp_ac_import[g] and sec_in[flight1] in gate_security_import[g] and sec_out[flight1] in \
+                gate_security_import[g]:
+            error.append(1)
+        else:
+            error.append(0)
+
+    if not 1 in error:
+        errorobj[
+            flight1] = 'This flight is not supported at the airport as the required gate does not excist! (AC_TYPE or combi with S/NS)'
+
+if not errorobj == {}:
+    raise Exception(errorobj)
 
 try:
 
@@ -30,28 +79,47 @@ try:
     model = gp.Model("Gate_Planning")
 
     # Create variables
-    x = model.addVars(availability, vtype=GRB.BINARY, name="x")
+    x = model.addVars(flight, gate, vtype=GRB.BINARY, name="x")
 
     # Create objective
-    model.setObjective(gp.quicksum(pax[f]*distance[g]*x[f,g] for f, g in availability), GRB.MINIMIZE)
+    model.setObjective(gp.quicksum(PAX[f] * distance[g] * x[f, g] for f in flight for g in gate), GRB.MINIMIZE)
 
     # Add constraints
-    model.addConstrs(x.sum(f,'*') == 1 for f in flight) # 1 gate for 1 flight
-    #model.addConstrs(x.sum('*',g) <= 1 for g in gate)   # max 1 flight per gate
+    model.addConstrs(x.sum(f, '*') == 1 for f in flight)  # 1 gate for 1 flight
 
+    for f1 in flight:
+        for f2 in flight:
 
-    for i in range(1, len(flight)):
-        for j in range(1, len(gate)):
-            if departure['Flight'+str(i)] > arrival['Flight'+str(i+1)]:
-                model.addConstr(x['Flight'+str(i),'Gate'+str(j)] + x['Flight'+str(i+1),
-                                                                      'Gate'+str(j)] <= 1)
+            # 2 flights can't be at the same gate at the same time
+            if ETD[f1] > ETA[f2] and ETA[f1] < ETD[f2] and not f1 == f2 and flight.index(f1) < flight.index(f2):
+                for g in gate:
+                    model.addConstr(1 * x[f1, g] + 1 * x[f2, g] <= 1)
 
+        LHS = 0
+        for g in gate:
+
+            # Gate should be compatible with aircraft
+            if not AC[f1] in comp_ac[g]:
+                model.addConstr(1 * x[f1, g] == 0)
+
+            if not sec_in[f1] in gate_security[g]:
+                model.addConstr(1 * x[f1, g] == 0)
+
+            if not sec_out[f1] in gate_security[g]:
+                model.addConstr(1 * x[f1, g] == 0)
+
+            # Assign flight to the right gate type
+            t = np.where(types == flight_type[f1])[0][0]
+            if gate_type[g] == types[t][1]:
+                LHS += x[f1, g]
+        model.addConstr(LHS == 1)
 
     # Optimize model
     model.optimize()
 
     for v in model.getVars():
-        print('%s %g' % (v.varName, v.x))
+        if not abs(v.x) == 0:
+            print('%s %g' % (v.varName, v.x))
 
     print('Obj: %g' % model.objVal)
 
